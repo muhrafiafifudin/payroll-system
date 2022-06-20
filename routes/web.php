8b577dd4d1975a836001ef0b2cfc96a017f1d370
Muhammad Rafi A <muhammad.rafi.afifudin99@gmail.com>
2022-06-20 19:47:27 +0700

    Auth route login

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    if (Auth::user()) {
+        return redirect()->route('dashboard');
+    }
     return view('auth.login');
 });
 
